970a530112592d43eb66893b214a4724b19e7014
David Enoragbon <david.enoragbon@decagon.dev>
2021-09-12 23:06:20 +0100

    Design Admin Welcome page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import AdminLogin from './pages/admin/AdminLogin/AdminLogin';
+import AdminPage from './pages/admin/AdminPage/AdminPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={AdminLogin} />
+        <Route path="/admin-page" component={AdminPage} />
       </Switch>
     </div>
   );
