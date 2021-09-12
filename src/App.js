b6184fb1c50ba9baf224903e011c829f84472622
David Enoragbon <david.enoragbon@decagon.dev>
2021-09-12 22:46:23 +0100

    Design Admin Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import AdminLogin from './pages/admin/AdminLogin/AdminLogin';
 
 function App() {
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <div className="App">
-      <h1>hello yourself</h1>
+      <Switch>
+        <Route exact path="/" component={AdminLogin} />
+      </Switch>
     </div>
   );
 }
