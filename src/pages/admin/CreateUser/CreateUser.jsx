b38d71f2e39ff215c7eebcf608b07858487332eb
Taiwo Ogunyemi <ogunyemitaiwojohn@gmail.com>
2021-09-20 08:30:02 +0100

    Fixed error in CreateUser | removed unused const

--- src/pages/admin/CreateUser/CreateUser.jsx
+++ src/pages/admin/CreateUser/CreateUser.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Logo from '../../../assets/CSRL Logo 1.png';
 import Note from '../../../assets/contact-form-svgrepo-com 1.png';
-import Logo from '../../../assets/CSRL Logo 1.png';
 import Button from '../../../components/Button/Button';
 import TextField from '../../../components/TextField/TextField';
 import './CreateUser.css';
@@ -11,49 +10,47 @@ const CreateUser = () => {
   const history = useHistory();
   const createUser = () => {
     history.push('/create-user-success');
-  const createUser = () => {
-    alert('hi');
-  };
-  return (
-    <div className="create-user-page">
-      <div className="create-user-card">
-        <div className="create-user-header">
-          <img src={Logo} alt="CSP" />
-          <img src={Note} alt="NOTE" className="note" />
-          <h5>Create User</h5>
-        </div>
+    return (
+      <div className="create-user-page">
+        <div className="create-user-card">
+          <div className="create-user-header">
+            <img src={Logo} alt="CSP" />
+            <img src={Note} alt="NOTE" className="note" />
+            <h5>Create User</h5>
+          </div>
 
-        <form className="create-user-form m-auto" onSubmit={createUser}>
-          <div className="d-flex justify-content-space-around">
-            <TextField
-              className="create-user-input"
-              label="Full Name"
-              placeholder="Full name"
-            />
-            <TextField
-              className=" create-user-input"
-              label="Email"
-              placeholder="email"
-            />
-          </div>
-          <div className=" d-flex mt-3">
-            <TextField
-              className="create-user-input"
-              label="Create Password for user"
-              placeholder="Enter password"
-            />
-            <TextField
-              className="create-user-input"
-              label="Role ID"
-              placeholder="Role ID"
-            />
-          </div>
-          <Button text="Create User" className="mt-4 create-user-btn" />
-          <p>Already have an account? Click here to login</p>
-        </form>
+          <form className="create-user-form m-auto" onSubmit={createUser}>
+            <div className="d-flex justify-content-space-around">
+              <TextField
+                className="create-user-input"
+                label="Full Name"
+                placeholder="Full name"
+              />
+              <TextField
+                className=" create-user-input"
+                label="Email"
+                placeholder="email"
+              />
+            </div>
+            <div className=" d-flex mt-3">
+              <TextField
+                className="create-user-input"
+                label="Create Password for user"
+                placeholder="Enter password"
+              />
+              <TextField
+                className="create-user-input"
+                label="Role ID"
+                placeholder="Role ID"
+              />
+            </div>
+            <Button text="Create User" className="mt-4 create-user-btn" />
+            <p>Already have an account? Click here to login</p>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 };
 
 export default CreateUser;
