3761b25bf30f5acb2e17fa060719f03993442ceb
David Enoragbon <david.enoragbon@decagon.dev>
2021-09-30 13:19:42 +0100

    Design stockbroker change of name corporates

--- src/pages/requestDashboard/requestTypes/requestForms/ChangeOfNameFormCorporate.jsx
+++ src/pages/requestDashboard/requestTypes/requestForms/ChangeOfNameFormCorporate.jsx
@@ -2,61 +2,32 @@ import "../Requests.css";
 // eslint-disable-next-line import/prefer-default-export
 export const ChangeOfNameFormCorporate = (
   <div className="left">
-    <div>
-      <label>Certificate of Incorporation from the CAC</label>
-      <div className="file-input">
-        <input type="file" />
-        <span className="button">SELECT</span>
-        <span className="label" data-js-label>
-          png, jpeg (=300KB)
-        </span>
-      </div>
+    <div className="requirements">
+      <hr className="horizontal-line" />
+      <ul className="ulllll">
+        <li>Certificate of Incorporation from the CAC</li>
+        <li>NSE & SEC Letters approving the change</li>
+        <li>Signature Mandate/Board Resolution</li>
+        <li>
+          Banker's Confirmation of Signature letter in the new name duly signed
+          by authorized signatories
+        </li>
+        <li>Request letter duly signed by authorized signatories</li>
+      </ul>
     </div>
-    <div>
-      <label>NSE & SEC Letters approving the change</label>
-      <div className="file-input">
-        <input type="file" />
-        <span className="button">SELECT</span>
-        <span className="label" data-js-label>
-          png, jpeg (=300KB)
-        </span>
-      </div>
-    </div>
-    <div>
-      <label>Signature Mandate/Board Resolution</label>
-      <div className="file-input">
-        <input type="file" />
-        <span className="button">SELECT</span>
-        <span className="label" data-js-label>
-          png, jpeg (=300KB)
-        </span>
-      </div>
-    </div>
-    <div>
-      <label>
-        Banker's Confirmation of Signature letter in the new name duly signed by
-        authorized signatories
-      </label>
-      <div className="file-input">
-        <input type="file" />
-        <span className="button">SELECT</span>
-        <span className="label" data-js-label>
-          png, jpeg (=300KB)
-        </span>
-      </div>
-    </div>
-    <div>
-      <label>Request letter duly signed by authorized signatories</label>
-      <div className="file-input">
-        <input type="file" />
-        <span className="button">SELECT</span>
-        <span className="label" data-js-label>
-          png, jpeg (=300KB)
-        </span>
-      </div>
+    <div className="file-input">
+      <input type="file" />
+      <span className="button">SELECT</span>
+      <span className="label" data-js-label>
+        png, jpeg (=300KB)
+      </span>
     </div>
 
-    <button>SUBMIT REQUEST</button>
+    <div className="btn-container">
+      <button style={{ marginTop: "40px" }} className="btn-container2">
+        SUBMIT REQUEST
+      </button>
+    </div>
   </div>
 );
 export const ChangeOfNameHeaderC = (
